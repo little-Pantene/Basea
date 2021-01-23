3ba8a1ae0d2f75e2f97187f9b3a3a9ef23b7cab2
little-Pantene <77447904+little-Pantene@users.noreply.github.com>
2021-01-23 22:37:39 +0800

    Update 结构体-人.c

--- 结构体-人.c
+++ 结构体-人.c
@@ -46,17 +46,34 @@
 //	return 0;
 //}
 
+//struct person
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int main()
+//{
+//	struct person A = { "潘婷",23 };
+//	struct person *a = &A;
+//	printf("%s\n", a->name);
+//	printf("%d\n", a->age);
+//	return 0;
+//}
+
 struct person
 {
 	char name[20];
 	int age;
+	char sex[15];
+	char id[15];
 };
 
 int main()
 {
-	struct person A = { "潘婷",23 };
+	struct person A = {"潘婷",23,"女","11540019"};
+	printf("姓名：%s\n 年龄：%d\n 性别：%s\n 学号：%s\n",A.name,A.age,A.sex,A.id);
 	struct person *a = &A;
-	printf("%s\n", a->name);
-	printf("%d\n", a->age);
+	printf("姓名：%s\n 年龄：%d\n 性别：%s\n 学号：%s\n",a->name, a->age, a->sex, a->id);
 	return 0;
 }
